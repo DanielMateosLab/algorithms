96669827280e964edfe2f668f79da705cca2a2c2
Daniel Mateos <daniel.mat.lab@gmail.com>
2023-03-08 22:27:28 +0100

    Minor renaming and docs for resume work

--- src/typescript/kokoBananas.ts
+++ src/typescript/kokoBananas.ts
@@ -2,19 +2,22 @@
 
 function minEatingSpeed(piles: number[], h: number): number {
   const sortedPiles = [...piles].sort((a, b) => b - a);
-  const extraRounds = h - piles.length;
+  const availableExtraRounds = h - piles.length;
   let k =
     sortedPiles[
-      extraRounds < piles.length - 1 ? extraRounds : piles.length - 1
+      availableExtraRounds < piles.length - 1
+        ? availableExtraRounds
+        : piles.length - 1
     ];
 
   const kIsValid = () => {
-    const superRounds = sortedPiles
-      .slice(0, extraRounds)
+    const usedExtraRounds = sortedPiles
+      .slice(0, availableExtraRounds)
       .map((p) => Math.ceil(p / k - 1))
       .reduce((a, b) => a + b, 0);
-    return superRounds <= extraRounds;
+    return usedExtraRounds <= availableExtraRounds;
   };
+  // TODO: used must equal available, so you have to decide when to add or subtract to K
 
   while (!kIsValid()) {
     k++;
