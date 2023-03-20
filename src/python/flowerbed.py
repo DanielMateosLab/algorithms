98385372806818ebde97168c2dea91e398db94d4
Daniel Mateos <daniel.mat.lab@gmail.com>
2023-03-20 21:00:24 +0100

    Solve flowerbed

--- src/python/flowerbed.py
+++ src/python/flowerbed.py
@@ -1,14 +1,23 @@
+# 605
 from typing import List
 
-""" 
-0 1 0 -> 3, 1, 1 
-0 1 0 0 -> 4, 2, 0 4 / 2 - 1 + 0
-0 1 0 1 0 -> 5, 2, 1 
-0 1 0 1 0 0 / 6, 3 - 1
-0 1 0 1 0 1 0 / 7 3 - 1 + 1
-"""
 
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if n == 0:
+            return True
+        self.flowerbed = flowerbed[:]
         count = 0
-        for item
+        for i, item in enumerate(flowerbed):
+            if item != 1:
+                if self.check_spot(i - 1) and self.check_spot(i + 1):
+                    self.flowerbed[i] = 1
+                    count += 1
+                    if count == n:
+                        return True
+        return False
+
+    def check_spot(self, i: int):
+        if i < 0 or i >= len(self.flowerbed):
+            return True
+        return self.flowerbed[i] == 0
